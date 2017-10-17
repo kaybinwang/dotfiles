1f71b81e20d2282e2dcda3b1241cb227a5071158
Kevin Wang <kaybinwang@gmail.com>
2017-10-16 20:37:10 -0700

    fix linking of dfsetup

--- util/brew.sh
+++ util/brew.sh
@@ -10,6 +10,8 @@ function brew_install() {
   local -r pkg="$1"
   if ! brew ls --versions "$pkg" >/dev/null; then
     brew install "$pkg"
+  else
+    print_warning "brew already has $pkg installed"
   fi
 }
 
@@ -17,5 +19,7 @@ function brew_cask_install() {
   local -r pkg="$1"
   if ! brew cask ls --versions "$pkg" >/dev/null; then
     brew cask install "$pkg"
+  else
+    print_warning "brew already has cask $pkg installed"
   fi
 }
