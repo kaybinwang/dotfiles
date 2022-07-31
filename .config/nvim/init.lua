7b8beefc1047d884ace94117aff9828b9c839fda
Kevin Wang <kaybinwang@gmail.com>
2022-07-31 13:50:09 -0400

    update nvim to exit early when bootstrapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,6 +86,19 @@ require("packer").startup(function(use)
    end
 end)
 
+-- When we are bootstrapping a configuration, it doesn't make sense to execute
+-- the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
 
 --==============================================================================
 -- 2. Editor Settings
