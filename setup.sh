8f0daf7935607f2a6a7595edd47f6bbc468a9eac
Kevin Wang <kaybinwang@gmail.com>
2022-08-01 00:07:28 -0400

    add setup script to automate install

---
+++ setup.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+abs_path() {
+  echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+}
+
+DOTFILES_PATH="$(dirname "$(abs_path "${BASH_SOURCE[0]}")")"
+
+DOTFILES=(
+  .bash_profile
+  .bashrc
+  .config/bat
+  .config/kitty
+  .config/nvim
+  .config/sh
+  .gitconfig
+  .gitconfig-personal
+  .gitconfig-work
+  .zshrc
+)
+
+REPOS=(
+  homebrew/cask-fonts
+)
+
+PACKAGES=(
+  bash
+  bash-completion
+  bash-language-server
+  bat
+  coreutils
+  fzf
+  git
+  kotlin-language-server
+  lua-language-server
+  neovim
+  shellcheck
+  zsh
+)
+
+CASKS=(
+  font-fira-code-nerd-font
+)
+
+if ! command -v brew; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew already exists. Skipping..."
+fi
+
+# TODO: automate installing Intel brew (only when using ARM)?
+
+echo "Tapping third-party repositories..."
+brew tap "${REPOS[@]}"
+
+echo "Installing packages..."
+brew install "${PACKAGES[@]}"
+
+echo "Installing casks..."
+
+brew install --cask "${CASKS[@]}"
+
+echo "Installing dotfiles..."
+for dotfile in "${DOTFILES[@]}"; do
+  src="$DOTFILES_PATH/$dotfile"
+  dst="$HOME/$dotfile"
+  if [ -h "$dst" ]; then
+    # safe to delete symlink
+    rm "$dst"
+  fi
+  echo "Creating symlink from $dst -> $src..."
+  ln -s "$src" "$dst"
+done
