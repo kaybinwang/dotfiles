c2dc7502201d87226ace2dd2fd3bc1214f0b7cb2
Kevin Wang <kaybinwang@gmail.com>
2022-08-01 20:01:08 -0400

    replace vim-gitgutter with gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,7 +80,7 @@ require("packer").startup(function(use)
    use "neovim/nvim-lspconfig"   -- Configurations for Nvim LSP
    use "tpope/vim-fugitive"
    use "tpope/vim-rhubarb"       -- GitHub extension for vim-fugitive
-   use "airblade/vim-gitgutter"  -- show modified hunks in column margins
+   use "lewis6991/gitsigns.nvim" -- show modified hunks in column margins
    use "tpope/vim-eunuch"        -- Vim sugar for UNIX commands
    use "janko-m/vim-test"
 
@@ -141,7 +141,7 @@ vim.opt.undolevels = 10000                            -- store 10000 undos
 
 -- 2.2.1 Theme
 -- vim.cmd "colorscheme onehalflight"
-require('github-theme').setup({
+require("github-theme").setup({
   theme_style = "light"
 })
 -- vim.opt.background = "light"
@@ -246,7 +246,7 @@ vim.keymap.set("n", "Y", "y$", { noremap = true })
 
 require("fzf-lua").setup({
   winopts = {
-    preview = { default = 'bat' }
+    preview = { default = "bat" }
   },
   files = {
     git_icons = false, -- disable since this degrades performance the most
@@ -272,9 +272,47 @@ vim.api.nvim_create_user_command(
 -- 3.6 Git
 --------------------------------------------------------------------------------
 
--- Move to next git modification
-vim.api.nvim_set_keymap("n", "<leader>gp", ":GitGutterPreviewHunk<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gu", ":GitGutterUndoHunk<cr>", { noremap = true, silent = true })
+-- Manage Git hunks
+require("gitsigns").setup({
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then return "]c" end
+      vim.schedule(function() gs.next_hunk() end)
+      return "<Ignore>"
+    end, {expr=true})
+
+    map("n", "[c", function()
+      if vim.wo.diff then return "[c" end
+      vim.schedule(function() gs.prev_hunk() end)
+      return "<Ignore>"
+    end, {expr=true})
+
+    -- Actions
+    map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+    map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+    map("n", "<leader>hS", gs.stage_buffer)
+    map("n", "<leader>hu", gs.undo_stage_hunk)
+    map("n", "<leader>hR", gs.reset_buffer)
+    map("n", "<leader>hp", gs.preview_hunk)
+    map("n", "<leader>hb", function() gs.blame_line{full=true} end)
+    map("n", "<leader>tb", gs.toggle_current_line_blame)
+    map("n", "<leader>hd", gs.diffthis)
+    map("n", "<leader>hD", function() gs.diffthis("~") end)
+    map("n", "<leader>td", gs.toggle_deleted)
+
+    -- Text object
+    map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+  end
+})
 
 -- Fugitive Git commands
 vim.keymap.set("n", "<leader>gbr", ":GBrowse master:%<cr>", { noremap = true, silent = true })
