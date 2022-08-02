d0abed8f5e50179b596db264d4a2343317008312
Kevin Wang <kaybinwang@gmail.com>
2022-08-02 00:14:38 -0400

    fix some styling in nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -113,12 +113,12 @@ end)
 --
 -- You'll need to restart nvim, and then it will work.
 if is_bootstrap then
-  print "=================================="
-  print "    Plugins are being installed"
-  print "    Wait until Packer completes,"
-  print "       then restart nvim"
-  print "=================================="
-  return
+   print "=================================="
+   print "    Plugins are being installed"
+   print "    Wait until Packer completes,"
+   print "       then restart nvim"
+   print "=================================="
+   return
 end
 
 
@@ -157,10 +157,10 @@ vim.opt.undolevels = 10000                            -- store 10000 undos
 --------------------------------------------------------------------------------
 
 -- 2.2.1 Theme
--- vim.cmd "colorscheme onehalflight"
 require("github-theme").setup({
-  theme_style = "light"
+   theme_style = "light"
 })
+-- vim.cmd "colorscheme onehalflight"
 -- vim.opt.background = "light"
 
 -- 2.2.2 Buffers
@@ -257,19 +257,22 @@ vim.keymap.set("n", "Y", "y$", { noremap = true })
 -- 3.4 Terminal
 --------------------------------------------------------------------------------
 
+-- Removed navigation remaps like <c-h> so that they work in the terminal.
+-- Instead, return to normal mode in order to access the window navigation
+-- mappings
 
 --------------------------------------------------------------------------------
 -- 3.5 File Searching
 --------------------------------------------------------------------------------
 
 require("fzf-lua").setup({
-  winopts = {
-    preview = { default = "bat" }
-  },
-  files = {
-    git_icons = false, -- disable since this degrades performance the most
-    file_icons = true,
-  }
+   winopts = {
+      preview = { default = "bat" }
+   },
+   files = {
+      git_icons = false, -- disable since this degrades performance the most
+      file_icons = true,
+   }
 })
 
 vim.keymap.set("n", "<leader>p", ":lua require('fzf-lua').files()<cr>", { noremap = true, silent = true })
@@ -292,44 +295,44 @@ vim.api.nvim_create_user_command(
 
 -- Manage Git hunks
 require("gitsigns").setup({
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
+   on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
+      local function map(mode, l, r, opts)
+         opts = opts or {}
+         opts.buffer = bufnr
+         vim.keymap.set(mode, l, r, opts)
+      end
 
-    -- Navigation
-    map("n", "]c", function()
-      if vim.wo.diff then return "]c" end
-      vim.schedule(function() gs.next_hunk() end)
-      return "<Ignore>"
-    end, {expr=true})
+      -- Navigation
+      map("n", "]c", function()
+         if vim.wo.diff then return "]c" end
+         vim.schedule(function() gs.next_hunk() end)
+         return "<Ignore>"
+      end, {expr=true})
 
-    map("n", "[c", function()
-      if vim.wo.diff then return "[c" end
-      vim.schedule(function() gs.prev_hunk() end)
-      return "<Ignore>"
-    end, {expr=true})
+      map("n", "[c", function()
+         if vim.wo.diff then return "[c" end
+         vim.schedule(function() gs.prev_hunk() end)
+         return "<Ignore>"
+      end, {expr=true})
 
-    -- Actions
-    map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-    map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-    map("n", "<leader>hS", gs.stage_buffer)
-    map("n", "<leader>hu", gs.undo_stage_hunk)
-    map("n", "<leader>hR", gs.reset_buffer)
-    map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function() gs.blame_line{full=true} end)
-    map("n", "<leader>tb", gs.toggle_current_line_blame)
-    map("n", "<leader>hd", gs.diffthis)
-    map("n", "<leader>hD", function() gs.diffthis("~") end)
-    map("n", "<leader>td", gs.toggle_deleted)
+      -- Actions
+      map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+      map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+      map("n", "<leader>hS", gs.stage_buffer)
+      map("n", "<leader>hu", gs.undo_stage_hunk)
+      map("n", "<leader>hR", gs.reset_buffer)
+      map("n", "<leader>hp", gs.preview_hunk)
+      map("n", "<leader>hb", function() gs.blame_line{full=true} end)
+      map("n", "<leader>tb", gs.toggle_current_line_blame)
+      map("n", "<leader>hd", gs.diffthis)
+      map("n", "<leader>hD", function() gs.diffthis("~") end)
+      map("n", "<leader>td", gs.toggle_deleted)
 
-    -- Text object
-    map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-  end
+      -- Text object
+      map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+   end
 })
 
 -- Fugitive Git commands
