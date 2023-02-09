5bf6c411ce4d1a33856bafd55d76f3268101fdb2
Kevin Wang <kaybinwang@gmail.com>
2023-02-09 16:58:13 -0500

    update brew script to handle intel case

--- .config/sh/load_brew.sh
+++ .config/sh/load_brew.sh
@@ -1,9 +1,11 @@
 # shellcheck shell=sh
 local arch=$(uname -p)
-if [ "$arch" = 'arm' ]; then
-  if [ -x /opt/homebrew/bin/brew ]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  else
-    __print_warning "Couldn't find an ARM installation of Homebrew."
-  fi
+if [ "$arch" = 'arm' ] && [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  return 0
 fi
+if [ "$arch" = 'i386' ] && [ -x /usr/local/bin/brew ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+  return 0
+fi
+__print_warning "Couldn't find installation of Homebrew for $arch."
