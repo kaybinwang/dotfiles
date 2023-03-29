2dd4fe9d568a9bdb43ccc1e0fd61d574920f9659
Kevin Wang <kaybinwang@gmail.com>
2023-03-29 03:19:38 -0400

    fix bash script warnings

--- .config/sh/load_brew.sh
+++ .config/sh/load_brew.sh
@@ -1,11 +1,16 @@
 # shellcheck shell=sh
-local arch=$(uname -p)
-if [ "$arch" = 'arm' ] && [ -x /opt/homebrew/bin/brew ]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  return 0
-fi
-if [ "$arch" = 'i386' ] && [ -x /usr/local/bin/brew ]; then
-  eval "$(/usr/local/bin/brew shellenv)"
-  return 0
-fi
-__print_warning "Couldn't find installation of Homebrew for $arch."
+
+load_brew() {
+  local arch=$(uname -p)
+  if [ "$arch" = 'arm' ] && [ -x /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    return 0
+  fi
+  if [ "$arch" = 'i386' ] && [ -x /usr/local/bin/brew ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+    return 0
+  fi
+  __print_warning "Couldn't find installation of Homebrew for $arch."
+}
+
+load_brew
