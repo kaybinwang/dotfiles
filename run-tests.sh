d0b1642df7034cfeffec3fdfeef131185d809115
Kevin Wang <kaybinwang@gmail.com>
2023-03-29 12:21:33 -0400

    Revert "add tests to workflow (#5)"
    This reverts commit 3ef17b745f4461ed722b1dc440726b7b649c5a77.

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source scripts/utils/log.sh
-
-log_info "Running all tests..."
-for script in $(find tests -name '*.exp'); do
-  log_info "Executing expect script: $script"
-  expect "$script"
-done
