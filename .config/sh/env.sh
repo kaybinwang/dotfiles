bb7f1f4b4c050d3f5713f22817863e2b24a710a1
Kevin Wang <kaybinwang@gmail.com>
2023-03-30 12:17:11 -0400

    fix typo

--- .config/sh/env.sh
+++ .config/sh/env.sh
@@ -5,7 +5,7 @@ export PERSONAL_PROJECTS="$HOME/projects/personal"
 export WORK_PROJECTS="$HOME/projects/work"
 
 nvim="$(__get_nvim_path_with_fallback)"
-if [ -z "$nvim" ]; then
+if [ -n "$nvim" ]; then
   export VISUAL="$nvim"
   export EDITOR="$VISUAL"
 fi
