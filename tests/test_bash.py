b1a8ebb0fe6ca75e02f869f578de96d72bbcd0e1
Kevin Wang <kaybinwang@gmail.com>
2023-03-29 21:04:54 -0400

    add unicode support

--- tests/test_bash.py
+++ tests/test_bash.py
@@ -4,10 +4,10 @@ import pexpect
 import time
 
 
+RED = b""
+GREEN = b"\x1b[0;32m"
 
-def test_start_prompt() -> None:
-    child = pexpect.spawn("bash")
-    prompt = (
-        b"\x1b[?2004h\x1b[0m(dotfiles) \x1b[0;32m[\xe2\x9c\x94]\x1b[0m \x1b[0;33mwang.kevin\x1b[0m@\x1b[0;32mKWang-M-K322P\x1b[0m:\x1b[0;34m~/projects/personal/dotfiles\x1b[0m \x1b[0;35m(fix-tests)\x1b[0m $ "
-    )
+def test_prompt_on_startup() -> None:
+    child = pexpect.spawn("bash", encoding="utf-8")
+    prompt = "\x1b[?2004h\x1b[0m(dotfiles) \x1b[0;32m[✔]\x1b[0m \x1b[0;33mwang.kevin\x1b[0m@\x1b[0;32mKWang-M-K322P\x1b[0m:\x1b[0;34m~/projects/personal/dotfiles\x1b[0m \x1b[0;35m(fix-tests)\x1b[0m $ "
     child.expect_exact([prompt], timeout=5)
