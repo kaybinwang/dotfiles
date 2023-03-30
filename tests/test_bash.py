b205d659cc2aea96aec6050b34551e3494fa3172
Kevin Wang <kaybinwang@gmail.com>
2023-03-29 21:54:26 -0400

    update test to read rc file

--- tests/test_bash.py
+++ tests/test_bash.py
@@ -11,6 +11,7 @@ YELLOW = "\x1b[0;33m"
 BLUE = "\x1b[0;34m"
 PURPLE = "\x1b[0;35m"
 RESET = "\x1b[0m"
+
 USER = os.environ["USER"]
 HOST = subprocess.run(['hostname'], capture_output=True, text=True).stdout.strip()
 CWD = os.getcwd()
@@ -21,7 +22,7 @@ def test_prompt_on_startup() -> None:
     cwd = CWD
     if cwd.startswith(HOME):
         cwd = "~" + CWD.removeprefix(HOME)
-    child = pexpect.spawn("bash", encoding="utf-8")
+    child = pexpect.spawn("bash --rcfile .bashrc", encoding="utf-8")
     prompt = (
         f"\x1b[?2004h{RESET}(dotfiles) "
         f"{GREEN}[✔]{RESET} "
