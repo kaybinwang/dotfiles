2dc49781af31add012a879c2e07158746ce70512
Kevin Wang <kaybinwang@gmail.com>
2023-03-29 21:15:29 -0400

    working

--- tests/test_bash.py
+++ tests/test_bash.py
@@ -7,9 +7,16 @@ import time
 RED = b""
 GREEN = "\x1b[0;32m"
 YELLOW = "\x1b[0;33m"
+BLUE = "\x1b[0;34m"
+PURPLE = "\x1b[0;35m"
 RESET = "\x1b[0m"
 
 def test_prompt_on_startup() -> None:
     child = pexpect.spawn("bash", encoding="utf-8")
-    prompt = f"\x1b[?2004h{RESET}(dotfiles) {GREEN}[✔]{RESET} {YELLOW}wang.kevin{RESET}@{GREEN}KWang-M-K322P{RESET}:\x1b[0;34m~/projects/personal/dotfiles\x1b[0m \x1b[0;35m(fix-tests)\x1b[0m $ "
+    prompt = (
+        f"\x1b[?2004h{RESET}(dotfiles) "
+        f"{GREEN}[✔]{RESET} "
+        f"{YELLOW}wang.kevin{RESET}@{GREEN}KWang-M-K322P{RESET}:{BLUE}~/projects/personal/dotfiles{RESET} "
+        f"{PURPLE}(fix-tests){RESET} $ "
+    )
     child.expect_exact([prompt], timeout=5)
